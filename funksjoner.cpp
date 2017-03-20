89b767975ac0b53bdebfad2418dcf7fa8420ccee
mats <moskjaer@stud.ntnu.no>
2017-03-20 14:14:24 +0100

    La til 2 les funksjoner i .h og .cpp

--- funksjoner.cpp
+++ funksjoner.cpp
@@ -1,16 +1,32 @@
-//funksjoner.cpp
+/*
+    funksjoner.cpp
+    Globale funksjoner
+*/
+////////////////////////////////////////////////////:://///////////////////////////////
 
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
-#endif 
+#endif
 
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
-/*
-    funksjoner.cpp
-    Globale funksjoner
-*/
-////////////////////////////////////////////////////:://///////////////////////////////
+
+
+char les() {                                        // Leser og returnerer ETT upcaset tegn.
+  char ch;
+  cout << "\n\nKommando:  ";
+  cin >> ch;  cin.ignore();
+  return (toupper(ch));
+}
+                                                    // Skriver ledetekst (t), leser og
+int les(char* t, int min, int max)  {               // returnerer et tall mellom min og max:
+  int tall;
+  do  {
+    cout << '\t' << t << " (" << min << '-' << max <<  "):  ";
+    cin >> tall;  cin.ignore();
+  } while (tall < min  ||  tall > max);
+  return tall;
+}
