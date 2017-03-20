9d4a483925b2ab5c819e455a110540dd0ffccd22
mats <moskjaer@stud.ntnu.no>
2017-03-20 14:35:13 +0100

    La til STRLEN, la til noen funksjoner

--- funksjoner.cpp
+++ funksjoner.cpp
@@ -22,11 +22,23 @@ char les() {                                        // Leser og returnerer ETT u
   return (toupper(ch));
 }
                                                     // Skriver ledetekst (t), leser og
-int les(char* t, int min, int max)  {               // returnerer et tall mellom min og max:
-  int tall;
-  do  {
-    cout << '\t' << t << " (" << min << '-' << max <<  "):  ";
-    cin >> tall;  cin.ignore();
-  } while (tall < min  ||  tall > max);
-  return tall;
+                                                    // returnerer et tall mellom min og max:
+int les(const char t[], const int min, const int max) {
+	int i;
+	char buffer[256];
+	do {                                            // Skriver ledetekst:
+		cout << '\t' << t << " (" << min << '-' << max << "): ";
+		fgets(buffer, 256, stdin);
+		i = atoi(buffer);
+	} while (i < min || i > max);                   // Sjekker at i lovlig intervall.
+	return i;                                       // Returnerer innlest tall.
+}
+
+
+char *konverter(char t[]) {                         // Konverterer og returnerer char [] til char*
+	char *string;
+                                                    // Konverterer char array til char *
+	string = new char[strlen(t) + 1];
+	strcpy(string, t);
+	return string;
 }
